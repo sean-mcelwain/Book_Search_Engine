86fac6c706b855f317eb5ccc0cce0573e9831d8a
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-08-12 22:17:43 -0500

    debugging

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require("../util/auth");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +14,11 @@ const server = new ApolloServer({
   context: authMiddleware,
 })
 
-server.applyMiddleware({ app });
+async function startServerWithMiddleware() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,6 +28,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
